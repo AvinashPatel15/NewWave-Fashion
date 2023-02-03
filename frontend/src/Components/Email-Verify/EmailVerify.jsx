a4663da094cb2c6c20fd5fd90b0480a70b7763d6
Avinash Patel <avinashpatel9755@gmail.com>
2023-02-04 03:34:18 +0530

    Updated in frontend emailverify page

--- frontend/src/Components/Email-Verify/EmailVerify.jsx
+++ frontend/src/Components/Email-Verify/EmailVerify.jsx
@@ -13,7 +13,7 @@ const EmailVerify = () => {
   useEffect(() => {
     const verifyEmailUrl = async () => {
       try {
-        const url = `http://localhost:8080/users/${param.id}/verify/${param.token}`;
+        const url = `https://new-wave-fashion-server.vercel.app/users/${param.id}/verify/${param.token}`;
         const { data } = await axios.get(url);
         setValidUrl(true);
       } catch (error) {
