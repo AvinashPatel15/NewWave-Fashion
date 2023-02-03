a875be5f79f05e161522974fb4146f4215335db2
Avinash Patel <avinashpatel9755@gmail.com>
2023-02-03 18:22:57 +0530

    Updated Wishlist Route File

--- Backend/Routes/Wishlist.Route.js
+++ Backend/Routes/Wishlist.Route.js
@@ -34,13 +34,12 @@ wishlistRouter.post("/post/:productID", UserAuth, async (req, res) => {
   const userID = req.userID;
   const productID = req.params.productID;
   try {
-    let isProduct = await wishlistRouter.findOne({ productID, userID });
+    let isProduct = await WishlistModel.findOne({ productID, userID });
     if (isProduct) {
-      res.status(401).send({ message: "This Product Is Already In Wishlist!" });
-    } else {
-      await wishlistRouter.create({ productID, userID });
-      res.send({ message: "Product Added To The Wishlist Successfully" });
+      res.status(401).send({ message: "This Item Is Already In Wishlist!" });
     }
+    await WishlistModel.create({ productID, userID });
+    res.send({ message: "Item Added To The Wishlist Successfully" });
   } catch (error) {
     res
       .status(401)
