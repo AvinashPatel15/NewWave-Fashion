6689320c529d65616fa31e4259b735b3b8c1f9e6
Avinash Patel <avinashpatel9755@gmail.com>
2023-02-06 00:02:27 +0530

    Added home page section 1 carousel

--- frontend/src/Pages/HomePage/Home.jsx
+++ frontend/src/Pages/HomePage/Home.jsx
@@ -1,7 +1,19 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import Section1Carousel from "../../Components/HomePage/Section1Carousel";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <Box width={"100%"} marginTop={{ base: 5, lg: 10 }}>
+        <Section1Carousel />
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
