8365142cb8abbae5751589bf11c769a041d5ca87
Avinash Patel <avinashpatel9755@gmail.com>
2023-02-07 23:03:24 +0530

    Added sign up and login page

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import EmailVerify from "../Pages/Email-Verify/EmailVerify";
 import Home from "../Pages/HomePage/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Sign-up/Register";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+      <Route path="/sign-up" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
