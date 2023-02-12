df5f977cab8325babea0c0c532909998f785b251
Avinash Patel <avinashpatel9755@gmail.com>
2023-02-12 20:14:48 +0530

    Updated Auth Redux

--- frontend/src/Redux/Login/Auth.Actions.js
+++ frontend/src/Redux/Login/Auth.Actions.js
@@ -2,10 +2,14 @@ import axios from "axios";
 
 export const Login = (loginData) => async (dispatch) => {
   try {
-    let { data } = axios.post(
-      "https://new-wave-fashion-server.cyclic.app/users/login",
-      loginData
-    );
+    let { data } = axios
+      .post("https://new-wave-fashion-server.cyclic.app/users/login", loginData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     console.log(data);
   } catch (error) {
     console.log(error);
